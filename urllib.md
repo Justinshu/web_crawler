609ed1dbe5c6547022b529175223e43f42afd2fb
樵夫 <hydrostaticwater@gmail.com>
2020-10-12 23:45:37 +0800

    home

--- urllib.md
+++ urllib.md
@@ -1,4 +1,4 @@
-# urllib库
+# urllib库中的request
 >在`python3`的`urllib`库中，所有和网络请求相关的方法都被集成到`urllib.request`模块下面
 
 ## urlopen
@@ -34,10 +34,133 @@ print(response.getcode())
 ## urlretrieve
 > def urlretrieve(url, filename=None, reporthook=None, data=None):
 
->方便将网页上面的一个文件保存到本地
+> 方便将网页上面的一个文件保存到本地
 ```python
 from urllib import request
 
+# url = 想要获取的网址      filename = 代表是下载好的文件文件位置和文件名称
 request.urlretrieve("https://www.baidu.com","baidu.html")
+# 下载铠爹的一张图片
+request.urlretrieve("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1602244848418&di=55f89cc9014c7439c658df9539cdd59d&imgtype=0&src=http%3A%2F%2F01imgmini.eastday.com%2Fmobile%2F20200210%2F20200210124632_dce5fc05ecccb056da40c72aa8c0fe52_1.jpeg","urllib库/kai.png")
 
 ```
+
+## request.Request类
+> 在你爬取一个网站的时候可以利用这个类设置浏览器的请求头，伪装浏览器运行，而不是直接使用urlopen让浏览器知道你是一个Python爬虫
+
+```python
+from urllib import request
+
+
+request.Request(url= "",headers=)
+
+request.urlopen()
+
+```
+
+
+
+
+
+
+
+
+
+# urllib库中的parse
+
+> 使用urllib库中的parse可以解析和编码数据
+
+## urlencode 编码
+> 对于底层ASCII码无法识别的中文字体进行编码
+
+```python
+from urllib import parse
+params = {
+    "name": "张三" ,
+    "age" : 18,
+    "sex" : "male"
+    }
+
+result = parse.urlencode(params)
+
+print(result)
+
+# name=%E5%BC%A0%E4%B8%89&age=18&sex=male
+```
+
+
+## parse_qs 解码
+> 对于编码后的数据进行解码
+
+```python
+from urllib import parse
+date = {
+    "name": "张三" ,
+    "age" : 18,
+    "sex" : "male"
+    }
+
+qs = parse.urlencode(date)
+print(qs)
+
+result = parse.parse_qs(qs)
+print(result)
+
+# name=%E5%BC%A0%E4%B8%89&age=18&sex=male
+# {'name': ['张三'], 'age': ['18'], 'sex': ['male']}
+
+```
+
+## urlparse和urlsplit 
+> 用来解析url每一个参数，从而获取到每个参数的值，区别是urlparse中多了一个params参数
+
+```python
+from urllib import parse
+
+url = "https://www.baidu.com/s;hello word?wd=hello word&username=123#date"
+
+result1 = parse.urlparse(url)
+result2 = parse.urlsplit(url)
+
+print(result1)
+print(result2)
+
+# ParseResult(scheme='https', netloc='www.baidu.com', path='/s', params='hello word', query='wd=hello word&username=123', fragment='date')
+# SplitResult(scheme='https', netloc='www.baidu.com', path='/s;hello word', query='wd=hello word&username=123', fragment='date')
+
+
+print("scheme：",result1.scheme)
+print("netloc：",result1.netloc)
+print("path：",result1.path)
+print("params：",result1.params)
+print("query：",result1.query)
+print("fragment：",result1.fragment)
+
+# scheme： https
+# netloc： www.baidu.com
+# path： /s
+# params： hello word
+# query： wd=hello word&username=123
+# fragment： date
+
+
+print("scheme：",result2.scheme)
+print("netloc：",result2.netloc)
+print("path：",result2.path)
+print("query：",result2.query)
+print("fragment：",result2.fragment)
+
+# scheme： https
+# netloc： www.baidu.com
+# path： /s;hello word
+# query： wd=hello word&username=123
+# fragment： date
+
+```
+
+
+
+
+
+
+
--- 测试.py
+++ 测试.py
@@ -1,10 +1,162 @@
-from urllib import request
+# from urllib import parse
+# date = {
+#     "name": "张三" ,
+#     "age" : 18,
+#     "sex" : "male"
+#     }
+#
+# qs = parse.urlencode(date)
+# print(qs)
+#
+# result = parse.parse_qs(qs)
+# print(result)
+#
 
-response = request.urlopen("https://www.baidu.com")
-request.urlretrieve("https://www.baidu.com","baidu.html")
-
+#
+# from urllib import parse
+#
+# url = "https://www.baidu.com/s;hello word?wd=hello word&username=123#date"
+#
+# result1 = parse.urlparse(url)
+# result2 = parse.urlsplit(url)
+# print(result1)
+# print(result2)
+# print("scheme：",result1.scheme)
+# print("netloc：",result1.netloc)
+# print("path：",result1.path)
+# print("params：",result1.params)
+# print("query：",result1.query)
+# print("fragment：",result1.fragment)
+#
+#
+# print("scheme：",result2.scheme)
+# print("netloc：",result2.netloc)
+# print("path：",result2.path)
+# print("query：",result2.query)
+# print("fragment：",result2.fragment)
+# name=%E5%BC%A0%E4%B8%89&age=18&sex=male
+# {'name': ['张三'], 'age': ['18'], 'sex': ['male']}
+# response = request.urlopen("https://www.baidu.com")
+# request.urlretrieve("https://www.baidu.com","baidu.html")
+# request.urlretrieve("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1602244848418&di=55f89cc9014c7439c658df9539cdd59d&imgtype=0&src=http%3A%2F%2F01imgmini.eastday.com%2Fmobile%2F20200210%2F20200210124632_dce5fc05ecccb056da40c72aa8c0fe52_1.jpeg","urllib库/kai.png")
 # print(response)
 # print(response.read())
 # print(response.readline())
 # print(response.readlines())
 # print(response.getcode())
+
+#
+import http.cookiejar
+from urllib import request,parse
+import time
+
+url = "https://www.lagou.com/jobs/positionAjax.json?city=%E4%B8%8A%E6%B5%B7&needAddtionalResult=false"
+
+form = {
+    "first": "true",
+    "pn": 1,
+    "kd": "Python"
+}
+
+headers = {
+    "origin": "https://www.lagou.com",
+    "referer": "https://www.lagou.com/jobs/list_Python?labelWords=&fromSearch=true&suginput=",
+    "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36",
+    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8'
+}
+
+cookie = http.cookiejar.CookieJar()
+handler = request.HTTPCookieProcessor(cookie)
+opener = request.build_opener(handler)
+# res = request.Request(url="https://www.lagou.com/jobs/list_python?city=%E5%85%A8%E5%9B%BD&cl=false&fromSearch=true&labelWords=&suginput=", headers=headers,data=parse.urlencode(form).encode("utf-8"),method="GET")
+# opener.open(url="https://www.lagou.com/jobs/list_python?city=%E5%85%A8%E5%9B%BD&cl=false&fromSearch=true&labelWords=&suginput=")
+resp = request.Request(url='https://www.lagou.com/jobs/list_Python?labelWords=&fromSearch=true&suginput=', headers=headers)
+response = opener.open(resp,data=parse.urlencode(form).encode("utf-8"))
+
+time.sleep(5)
+
+resp = request.Request(url, headers=headers)
+response1 = opener.open(resp,data=parse.urlencode(form).encode("utf-8"))
+# res = request.urlopen(resp,timeout=5)
+
+# result= request.urlopen(resp,timeout=5)
+
+print(response1.read().decode("utf-8"))
+# import json
+# import requests
+# import xlwt
+# import time
+#
+#
+# # 获取存储职位信息的json对象，遍历获得公司名、福利待遇、工作地点、学历要求、工作类型、发布时间、职位名称、薪资、工作年限
+# def get_json(url, datas):
+#     my_headers = {
+#         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36",
+#         "Referer": "https://www.lagou.com/jobs/list_Python?city=%E5%85%A8%E5%9B%BD&cl=false&fromSearch=true&labelWords=&suginput=",
+#         "Content-Type": "application/x-www-form-urlencoded;charset = UTF-8"
+#     }
+#     time.sleep(5)
+#     ses = requests.session()  # 获取session
+#     ses.headers.update(my_headers)  # 更新
+#     ses.get(
+#         "https://www.lagou.com/jobs/list_python?city=%E5%85%A8%E5%9B%BD&cl=false&fromSearch=true&labelWords=&suginput=")
+#     content = ses.post(url=url, data=datas)
+#     result = content.json()
+#     info = result['content']['positionResult']['result']
+#     info_list = []
+#     for job in info:
+#         information = []
+#         information.append(job['positionId'])  # 岗位对应ID
+#         information.append(job['city'])  # 岗位对应城市
+#         information.append(job['companyFullName'])  # 公司全名
+#         information.append(job['companyLabelList'])  # 福利待遇
+#         information.append(job['district'])  # 工作地点
+#         information.append(job['education'])  # 学历要求
+#         information.append(job['firstType'])  # 工作类型
+#         information.append(job['formatCreateTime'])  # 发布时间
+#         information.append(job['positionName'])  # 职位名称
+#         information.append(job['salary'])  # 薪资
+#         information.append(job['workYear'])  # 工作年限
+#         info_list.append(information)
+#         # 将列表对象进行json格式的编码转换,其中indent参数设置缩进值为2
+#         # print(json.dumps(info_list, ensure_ascii=False, indent=2))
+#     # print(info_list)
+#     return info_list
+#
+#
+# def main():
+#     page = int(input('请输入你要抓取的页码总数：'))
+#     # kd = input('请输入你要抓取的职位关键字：')
+#     # city = input('请输入你要抓取的城市：')
+#
+#     info_result = []
+#     title = ['岗位id', '城市', '公司全名', '福利待遇', '工作地点', '学历要求', '工作类型', '发布时间', '职位名称', '薪资', '工作年限']
+#     info_result.append(title)
+#     for x in range(1, page + 1):
+#         url = 'https://www.lagou.com/jobs/positionAjax.json?needAddtionalResult=false'
+#         datas = {
+#             'first': 'false',
+#             'pn': x,
+#             'kd': 'python',
+#         }
+#         try:
+#             info = get_json(url, datas)
+#             info_result = info_result + info
+#             print("第%s页正常采集" % x)
+#         except Exception as msg:
+#             print("第%s页出现问题" % x)
+#
+#         # 创建workbook,即excel
+#         workbook = xlwt.Workbook(encoding='utf-8')
+#         # 创建表,第二参数用于确认同一个cell单元是否可以重设值
+#         worksheet = workbook.add_sheet('lagouzp', cell_overwrite_ok=True)
+#         for i, row in enumerate(info_result):
+#             # print(row)
+#             for j, col in enumerate(row):
+#                 # print(col)
+#                 worksheet.write(i, j, col)
+#         workbook.save('lagouzp.xls')
+#
+#
+# if __name__ == '__main__':
+#     main()
